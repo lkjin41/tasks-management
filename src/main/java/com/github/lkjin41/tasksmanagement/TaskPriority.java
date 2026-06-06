ebcb283d4b02bc0df693d31fb80a6f53473efa2e
lkjin41 <mihailfriskin@gmail.com>
2026-06-06 09:17:54 +0300

    add Task, TaskPriority, and TaskStatus classes for task management

---
+++ src/main/java/com/github/lkjin41/tasksmanagement/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.github.lkjin41.tasksmanagement;
+
+public enum TaskPriority {
+    Low, Medium, High
+}
---
+++ src/main/java/com/github/lkjin41/tasksmanagement/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.github.lkjin41.tasksmanagement;
+
+public enum TaskStatus {
+    CREATED, IN_PROGRESS, DONE
+}
+
+
