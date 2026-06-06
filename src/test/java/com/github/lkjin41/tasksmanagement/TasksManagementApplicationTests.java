b52c864f7ce53c5906bd57a186c7b91ef6b08231
lkjin41 <mihailfriskin@gmail.com>
2026-06-06 09:06:49 +0300

    init

---
+++ src/test/java/com/github/lkjin41/tasksmanagement/TasksManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lkjin41.tasksmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
