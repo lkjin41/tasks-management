afe61988923c76c836030dc1173c1c2844532e6e
lkjin41 <mihailfriskin@gmail.com>
2026-06-19 23:07:58 +0300

    feat(TasksManagementApplicationTests): add unit test for getAllTasks method in TaskService with search filter

--- src/test/java/com/github/lkjin41/tasksmanagement/TasksManagementApplicationTests.java
+++ src/test/java/com/github/lkjin41/tasksmanagement/TasksManagementApplicationTests.java
@@ -1,5 +1,6 @@
 package com.github.lkjin41.tasksmanagement;
 
+import com.github.lkjin41.tasksmanagement.tasks.controller.dto.task.TaskSearchFilter;
 import com.github.lkjin41.tasksmanagement.tasks.repository.TaskRepository;
 import com.github.lkjin41.tasksmanagement.tasks.service.TaskService;
 import com.github.lkjin41.tasksmanagement.tasks.task.*;
@@ -9,11 +10,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
@@ -42,7 +46,7 @@ class TasksManagementApplicationTests {
                 LocalDateTime.now().plusDays(5),
                 null,
                 TaskPriority.Low
-                );
+        );
         TaskEntity taskEntity = new TaskEntity(
                 TaskPriority.Low,
                 task.getDeadlineTime(),
@@ -64,7 +68,7 @@ class TasksManagementApplicationTests {
     }
 
     @Test
-    void getTaskById_ThrowsException_WhenTaskNotFound () {
+    void getTaskById_ThrowsException_WhenTaskNotFound() {
 
         Long taskId = 99L;
 
@@ -75,6 +79,40 @@ class TasksManagementApplicationTests {
                 () -> taskService.getTaskById(taskId)
         );
 
+    }
+
+    @Test
+    void getAllTasks_ReturnsMappedTasks() {
+        TaskSearchFilter taskSearchFilter = new TaskSearchFilter(
+                null,
+                null,
+                null,
+                null,
+                10,
+                0
+        );
+
+        var pageable = Pageable
+                .ofSize(10)
+                .withPage(0);
+
+        Task task = mock(Task.class);
+        TaskEntity taskEntity = new TaskEntity();
+
+        when(taskRepository.searchAllByFilter(
+                taskSearchFilter.creatorId(),
+                taskSearchFilter.assignedUserId(),
+                taskSearchFilter.status(),
+                taskSearchFilter.priority(),
+                pageable
+        )).thenReturn(List.of(taskEntity));
+
+        when(taskMapper.toTask(taskEntity)).thenReturn(task);
+
+        List<Task> result = taskService.getAllTasks(taskSearchFilter);
+
+        Assertions.assertEquals(1, result.size(), "Should return list with 1 element");
+        Assertions.assertEquals(task, result.get(0), "Task should match the mapped result");
     }
 
 }
